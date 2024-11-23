922a425e0de02db964e033a5cb632f749d1019d1
Dari <dariaberozkina.zip@gmail.com>
2024-11-23 13:57:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+anime({
+    targets: '.text-to-animate',
+    color: ['#000', '#FF6347'], // Зміна кольору від чорного до томатного
+    easing: 'easeInOutQuad',
+    duration: 3000, // Тривалість анімації 3 секунди
+    loop: true, // Анімація буде повторюватися
+    direction: 'alternate' // Кольори будуть чергуватися
+});
