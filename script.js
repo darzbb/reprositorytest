65031fab05424c2ead81cb4c3164d64247014c4c
Dari <dariaberozkina.zip@gmail.com>
2024-11-23 14:38:34 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-anime({
-    targets: '.text-to-animate',
-    color: ['#000', '#FF6347'], // Зміна кольору від чорного до томатного
-    easing: 'easeInOutQuad',
-    duration: 3000, // Тривалість анімації 3 секунди
-    loop: true, // Анімація буде повторюватися
-    direction: 'alternate' // Кольори будуть чергуватися
-});
